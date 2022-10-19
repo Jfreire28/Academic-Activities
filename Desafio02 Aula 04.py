d2bf01d2bc20ad6a1c40c87bcdbee0c7132ac7cc
Juliano <jfreire2804@gmail.com>
2022-10-18 23:44:52 -0300

    Python Activities

---
+++ Desafio02 Aula 04.py
@@ -0,0 +1,7 @@
+Primeiro_número = input('Primeiro número')
+Segundo_número = input('Segundo número')
+Soma = int(Primeiro_número)+int(Segundo_número)
+print('A soma é',Soma)
+
+
+
---
+++ Exercício Aula 04.py
@@ -0,0 +1,9 @@
+nome = input('Qual é o seu nome?')
+print('Bem-Vindo',nome)
+idade = input('Qual é a sua idade?')
+print('Você aparenta mais jovial do que sua idade de',idade)
+peso = input('Qual é o seu peso?')
+print('Você precisa se alimentar adequadamente para melhorar este peso de',peso)
+
+
+
