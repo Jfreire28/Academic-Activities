8f29ea6822fd143b74d8dc0f1406356334a3c448
Juliano <jfreire2804@gmail.com>
2022-10-18 23:35:05 -0300

    My first PyCharm activities

---
+++ Amodio Lessons.py
@@ -0,0 +1,13 @@
+#Aula Amodio
+
+numero01 = int(input('digite o primeiro numero'))
+numero02 = int(input('digite o segundo numero'))
+total = numero01+numero02
+print(' {} é o resultado da soma ente {}+{}'.format(total,numero01,numero02))
+
+
+
+
+
+
+
